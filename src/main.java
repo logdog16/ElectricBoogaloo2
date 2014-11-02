05188381c62a7c19ef88402f02c3380eee8fe33e
logdog16 <logandorsey@gmail.com>
2014-11-01 21:45:23 -0500

    Init Master

--- src/main.java
+++ src/main.java
@@ -1,5 +1,21 @@
 import rxtxrobot.*;
 
 public class main {
-
+	
+	
+	public static void main()
+	{
+	//Create a new Arduino Object
+	RXTXRobot master = new ArduinoNano(); 
+	
+	// Set port to COM3 for the first Arduino
+	master.setPort("COM3");
+	
+	//Connect to Master
+	master.connect();
+	
+	
+	}
+	
+	
 }
