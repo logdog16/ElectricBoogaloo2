e8472c12d3818e4570a19daf7c4f575564f4a965
Qwiggz <canon@canonellis.com>
2014-11-01 22:22:26 -0600

    Update 1

--- src/main.java
+++ src/main.java
@@ -8,13 +8,28 @@ public class main {
 	//Create a new Arduino Object
 	RXTXRobot master = new ArduinoNano(); 
 	
+	RXTXRobot slave = new ArduinoNano();
+	
 	// Set port to COM3 for the first Arduino
 	master.setPort("COM3");
+	slave.setPort("COM5");
 	
 	//Connect to Master
 	master.connect();
+	slave.connect();
+		for(int x = 0; x <= 300; x++){
+		 
+			if(x < 100){
+				master.runMotor(RXTXRobot.MOTOR1, 100, RXTXRobot.MOTOR2, 100, 0);
+			}else if( x == 100){
+				slave.runMotor(RXTXRobot.MOTOR1, 100, 20);
+			}else{
+				System.out.print("Some Crazy Stuff Has Happend to get here!");
+			}
+			
+		}
 	
-	//LOGAN IS A FURRY KING
+	
 	}
 	
 	
